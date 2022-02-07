f30777c946f5ff8642cbf8a3e5c5c9c43db56979
unknown <ovic391@gmail.com>
2022-02-07 21:23:14 +0200

    testing eb

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udacitynd2-env.eba-m6farddp.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://Udacitynd2-env.eba-m6farddp.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
